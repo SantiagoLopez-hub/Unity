039a04dfce31381f4576db3ea4770d467fb3a76e
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-05 10:19:52 +0000

    Creating triggers

--- Delivery Driver/Assets/Collision.cs
+++ Delivery Driver/Assets/Collision.cs
@@ -6,5 +6,10 @@ public class Collision : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!!");
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger");
     }
 }
