39cda08d572a3e96b9be0b3b9c34bcb5910ec2a6
SantiagoLopez-hub <silp4477@gmail.com>
2022-03-05 13:22:05 +0000

    Get game packages and deliver them to customers

--- Delivery Driver/Assets/Delivery.cs
+++ Delivery Driver/Assets/Delivery.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+    [SerializeField] float timeToDelete = 0.01f;
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!!");
-        
     }
     
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Trigger");
+        if (other.tag == "Package" && !hasPackage){
+            Debug.Log(other.tag + " picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, timeToDelete);
+        }
+        
+        if(other.tag == "Customer" && hasPackage){
+            Debug.Log("Delivered to " + other.tag);
+            hasPackage = false;
+        }
     }
 }
